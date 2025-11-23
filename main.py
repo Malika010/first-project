81aa77cbc510fe833d24aee778736e3716f9e765
MM <malikamahmoud2010@gmail.com>
2025-11-23 15:51:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Simple calculator
+# main.py
 def add(a, b):
   return a + b 
 def subtract (a, b):
@@ -6,11 +6,16 @@ def subtract (a, b):
 def multiply(a, b):
   return a * b
 def divide(a, b):
-  return a / b
+  if b ==0:
+    return float('inf') # or any way to handle divide by zero
+    return a / b
+    
+# Only run this block if this file is executed directly
 print("Select operation +, -, *, /")
 operation = input("Enter operation: ")
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+
 if operation == '+':
 print("Result" , add(num1, num2))
 elif operation == '-':
