bd4f5b3a71a36f3fe447d4f09aa8ca3e38cee5f3
Malika010 <malikamahmoud2010@gmail.com>
2025-11-23 15:13:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("My First Project")
+# Simple calculator
+def add(a, b):
+  return a + b 
+def subtract (a, b):
+  return a - b
+def multiply(a, b):
+  return a * b
+def divide(a, b):
+  return a / b
+print("Select operation +, -, *, /")
+operation = input("Enter operation: ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+if operation == '+':
+print("Result" , add(num1, num2))
+elif operation == '-':
+print("Result" , subtract(num1,  num2))
+elif operation == '*':
+print("Result" . multiply(num1, num2))
+elif operation == '/':
+print("Result" , divide(num1, num2))
+else:
+print("Invalid opertaion")
